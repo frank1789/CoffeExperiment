1ae8eadc0bdbeb022735b36f67469eb0804a5ef2
frank1789 <francesco.argentieri89@gmail.com>
2017-08-07 17:09:33 +0200

    :bug: fix import file, add symbols replacement

--- ExperimentWeight.R
+++ ExperimentWeight.R
@@ -7,6 +7,30 @@ getwd()
 setwd('/Users/francescoargentieri/ProjectR')
 
 df <- read.table("DesignMatrix-ResultWeight.dat", header = T)
+attributes(df$WaterType )
+
+df$WaterType <- factor(df$WaterType, levels=c('Levissima','SanBenedetto'),
+                 labels=c('-','+'))    
+ attributes(df$WaterType)
+ 
+ df$Pressing <- factor(df$Pressing, levels=c('No','Yes'),
+                        labels=c('-','+'))
+ df$Heat<- factor(df$Heat, levels=c('Low','High'),
+                   labels=c('-','+'))
+ 
+names(df)
+length(df)
+df <-df[3:(length(df))]
+df
+
+
+  names(df)[1] <- "A"
+  names(df)[2] <- "B"
+  names(df)[3] <- "C"
+  names(df)[4] <- "D"
+  names(df)[5] <- "E"
+
+df
 
 # define level
 lvl = c(-1, +1)
@@ -68,6 +92,7 @@ YieldWeight <-
 
 # complte the dataframe
 (df <- data.frame(df, Yield = YieldWeight))
+df
 
 # analisys of linear model
 df.lm   <- lm(Yield ~ A * B * C * D * E, d = df)
@@ -78,26 +103,27 @@ anova(df.lm)
 # Extract the effects by discarding the "Intercept" values
 effects <- as.vector(df.lm$effects)[2:length(df.lm$effects)]
 qn      <- qqnorm(effects,
-                  datax = T,
-                  ylab = "Effects quantiles",
+                  
+                  ylab = "Effects Quantiles",
                   main = "Normal probability plot")
 text(qn$x, qn$y, lab = names(df.lm$effects)[2:length(df.lm$effects)], pos = 4 )
-qqline(effects, datax = T, col = "red")
-
+qqline(effects,  col = "red")
+################################# fin qui ok
 # modify the linear model
-df.lm2 <- lm(Yield ~ A * C + B + D + E, data = df)
-qqnorm(df.lm2$res)
+df.lm2 <- lm(Yield ~ A * C + E + D, data = df)
+qqnorm(df.lm2$residuals)
+qqline(df.lm2$residuals)
 anova(df.lm2)
 
-
+plot(df.lm2$fit, df.lm2$res, xlab = "residuals", ylab = "fittedvalue")
 #df.lm2 <- lm(Yield~A+D*E, d = df1)
 #anova(df.lm2)
 
 # Guardo se il modello ? adeguato con qqplot
 
 
-qqnorm(df.lm2$residuals, datax = T, main = "QQ-plot residuals")
-qqline(df.lm2$residuals, datax = T)
+qqnorm(df.lm2$residuals, main = "QQ-plot residuals")
+qqline(df.lm2$residuals, )
 
 plot(df.lm2$fitted.values, df.lm2$residuals , ylab = "Residuals" , xlab = "Fitted",
      main = "Fitted values pattern")
@@ -105,3 +131,7 @@ plot(df.lm2$fitted.values, df.lm2$residuals , ylab = "Residuals" , xlab = "Fitte
 hist(df.lm2$residuals, xlab = "Residuals", main = "Histogram of residuals")
 close.screen(all.screens = T)
 anova(df.lm2)
+shapiro.test(df.lm2$residuals)
+qqnorm(df.lm2$residuals, datax = T, main = "QQ-plot residuals")
+qqline(df.lm2$residuals, datax = T)
+interaction.plot(df$E,df$D,df$Yield, xlab = "A", ylab = "B")
