d17ed93a9e68b2cbd22021d549528a62d02ade0e
frank1789 <francesco.argentieri89@gmail.com>
2017-08-07 12:54:13 +0200

    :bug: fix error

--- ExperimentWeight.R
+++ ExperimentWeight.R
@@ -6,59 +6,86 @@ close.screen(all = TRUE)
 getwd()
 setwd('/Users/francescoargentieri/ProjectR')
 
-
-library(lubridate)
-times <- c("00:00", "05:10", "07:15", "09:00", "11:30")
-ms(times)
-## [1] "0S"      "5M 10S"  "7M 15S"  "9M 0S"   "11M 30S"
-You can then use as.period() to convert to the time unit you desire:
-  
-  as.period(ms(times), unit = "sec")
-## [1] "0S"   "310S" "435S" "540S" "690S"
-If you convert to numeric now, you will get the seconds as numbers:
-  
-  seconds <- as.numeric(as.period(ms(times), unit = "sec"))
-seconds
-## [1]   0 310 435 540 690
-
-
 df <- read.table("DesignMatrix-ResultWeight.dat", header = T)
-
-
 
 # define level
 lvl = c(-1, +1)
+
 # define five factor
 factors = list(
   A = lvl,
- B = lvl,
+  B = lvl,
   C = lvl,
   D = lvl,
   E = lvl
 )
 
 # Definisco la design matrix
-df  <- expand.grid(A = lvl, B = lvl, C = lvl, D = lvl, E = lvl)
-df
-#data <- df$Yield
+df  <- expand.grid(
+  A = lvl,
+  B = lvl,
+  C = lvl,
+  D = lvl,
+  E = lvl
+)
 
-YieldWeight <- c(NA,NA,NA,NA,NA,NA,12,46,NA,NA,NA,NA,100,84,NA,NA,NA,132,106,NA,NA,NA,NA,NA,NA,NA,NA,134,NA,NA,NA,106) 
+# result yield experimet
+YieldWeight <-
+  c(
+    NA,
+    NA,
+    NA,
+    NA,
+    NA,
+    NA,
+    12,
+    46,
+    NA,
+    NA,
+    NA,
+    NA,
+    100,
+    84,
+    NA,
+    NA,
+    NA,
+    132,
+    106,
+    NA,
+    NA,
+    NA,
+    NA,
+    NA,
+    NA,
+    NA,
+    NA,
+    134,
+    NA,
+    NA,
+    NA,
+    106
+  )
 
+# complte the dataframe
 (df <- data.frame(df, Yield = YieldWeight))
-# Metodo di Daniels
 
-df.lm   <- lm(Yield~A*B*C*D*E, d = df)
-qqnorm(df.lm$res)
+# analisys of linear model
+df.lm   <- lm(Yield ~ A * B * C * D * E, d = df)
 anova(df.lm)
-# Estraggo gli effetti (escludendo l'intercetta)
+# the residual is equal to 0 then apply the Daniels method
+
+# Daniels method
+# Extract the effects by discarding the "Intercept" values
 effects <- as.vector(df.lm$effects)[2:length(df.lm$effects)]
-qn      <- qqnorm(effects, datax = T, ylab = "Effects quantiles",
+qn      <- qqnorm(effects,
+                  datax = T,
+                  ylab = "Effects quantiles",
                   main = "Normal probability plot")
 text(qn$x, qn$y, lab = names(df.lm$effects)[2:length(df.lm$effects)], pos = 4 )
-qqline(effects, datax = T)
-grid()
+qqline(effects, datax = T, col = "red")
 
-df.lm2 <- lm(Yield~A*C + D +E, data = df)
+# modify the linear model
+df.lm2 <- lm(Yield ~ A * C + B + D + E, data = df)
 qqnorm(df.lm2$res)
 anova(df.lm2)
 
